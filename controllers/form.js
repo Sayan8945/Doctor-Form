c6a7894c4c1dacc5605504f62618b838131b25a9
Sayan8945 <sayanpub2020@gmail.com>
2025-04-14 22:46:07 +0530

    remove date

--- controllers/form.js
+++ controllers/form.js
@@ -8,9 +8,6 @@ module.exports.data = async (req,res)=>{
     let user = req.body.user;
     let size = await Patient.countDocuments("");
     user.token = size + 1;
-    const currentDate = new Date();
-    const currentTime = currentDate.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true });
-    user.time = currentTime;
     let patient;
     await Patient.insertOne(user).then(res => {patient = res});  
     res.redirect(`/patient/${patient._id}`);
@@ -19,6 +16,5 @@ module.exports.data = async (req,res)=>{
 module.exports.user = async (req,res)=>{
     let {id} = req.params;
     let patient = await Patient.findById(id);
-    console.log(patient);
     res.render("user", {patient});
 }
